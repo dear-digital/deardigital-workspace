df1ea08c44a53cd85b7409c6852997623990fcec
michiel vandewalle <michiel@deardigital.com>
2023-04-07 21:31:29 +0200

    Fix errors with storyblok

--- libs/shared/ui/src/lib/content-blocks/featured-work.tsx
+++ libs/shared/ui/src/lib/content-blocks/featured-work.tsx
@@ -10,9 +10,11 @@ export function ContentFeaturedWork(props: ContentFeaturedWorkProps) {
   return (
     <Section {...props.section}>
       <div className="container" {...storyblokEditable(props as any)}>
-        <div className="row justify-content-center">
-          <div className="col-12 col-md-10 col-lg-8" dangerouslySetInnerHTML={{ __html: renderRichText(props.text) }} />
-        </div>
+        {props.text && (
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-10 col-lg-8" dangerouslySetInnerHTML={{ __html: renderRichText(props.text) }} />
+          </div>
+        )}
         <div className="row">
           {props.work.map((work, key) => (
             <div key={key} className="col-12 col-md-6 col-lg-4">
@@ -21,7 +23,7 @@ export function ContentFeaturedWork(props: ContentFeaturedWorkProps) {
           ))}
         </div>
       </div>
-    </Section>
+    </Section >
   );
 }
 
--- libs/shared/ui/src/lib/content-blocks/text-image.tsx
+++ libs/shared/ui/src/lib/content-blocks/text-image.tsx
@@ -34,9 +34,11 @@ export function ContentTextImage(props: ContentTextImageProps) {
   return (
     <Section {...props.section}>
       <div className="container" {...storyblokEditable(props as any)}>
-        <div className="row justify-content-center">
-          <div className="col-12 col-md-10 col-lg-8" dangerouslySetInnerHTML={{ __html: renderRichText(props.text) }} />
-        </div>
+        {props.text && (
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-10 col-lg-8" dangerouslySetInnerHTML={{ __html: renderRichText(props.text) }} />
+          </div>
+        )}
       </div>
     </Section>
   )
