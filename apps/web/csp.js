f5b1ed22e66a5d8a57451c5c95448340fd2a3d90
michiel vandewalle <michiel@deardigital.com>
2023-04-07 21:31:35 +0200

    add scp

--- apps/web/csp.js
+++ apps/web/csp.js
@@ -1,5 +1,9 @@
 const contentSecurityPolicy = `
-  default-src 'self';
+  default-src 'self' 'unsafe-eval';
+  img-src 'self' data: https://a.storyblok.com;
+  connect-src 'self' https://vitals.vercel-insights.com;
+  style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;
 `;
 
 module.exports = contentSecurityPolicy;
