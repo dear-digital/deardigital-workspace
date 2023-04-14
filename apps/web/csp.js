3460027fe34d9a79d5822266725f600cffae361a
michiel vandewalle <michiel@deardigital.com>
2023-04-14 15:33:08 +0200

    Add csp's

--- apps/web/csp.js
+++ apps/web/csp.js
@@ -1,8 +1,9 @@
 const contentSecurityPolicy = `
   default-src 'self' 'unsafe-eval';
   img-src 'self' data: https://a.storyblok.com;
-  connect-src 'self' https://vitals.vercel-insights.com;
+  connect-src 'self' https://vitals.vercel-insights.com https://api.storyblok.com;
   style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  style-src 'self' 'unsafe-inline';
   font-src 'self' https://fonts.gstatic.com;
 `;
 
