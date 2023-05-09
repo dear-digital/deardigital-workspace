ad212831a4c1407ef03535668cc9253768485a04
michiel vandewalle <michiel@deardigital.com>
2023-05-09 15:37:06 +0200

    Map navigation

--- libs/shared/mapper/src/lib/global/global.ts
+++ libs/shared/mapper/src/lib/global/global.ts
@@ -1,7 +1,8 @@
-import { GlobalInterface } from '@deardigital/shared/interfaces';
+import { GlobalInterface, LinkInterface } from '@deardigital/shared/interfaces';
 import { GlobalStoryblok } from '@deardigital/shared/schema';
 import { StoryblokStory } from 'storyblok-generate-ts';
 import { linkMapper } from '../link/link';
+import { Link } from 'd3';
 
 export function globalsMapper(globals?: StoryblokStory<GlobalStoryblok>): GlobalInterface | null {
   if (!globals) return null;
@@ -12,7 +13,14 @@ export function globalsMapper(globals?: StoryblokStory<GlobalStoryblok>): Global
     _uid: globals.content._uid,
     component: globals.content.component,
     header: {
-      navigation: globals?.content?.header?.[0].navigation?.map((nav) => linkMapper(nav.link, nav?.label)) ?? [],
+      navigation: globals?.content?.header?.[0].navigation?.reduce(
+        (acc, nav) => {
+          const item = linkMapper(nav.link, nav?.label);
+          if (item) {
+            acc.push(item)
+          }
+          return acc
+        }, [] as LinkInterface[]) ?? [],
       cta: globals?.content?.header?.[0].cta?.[0] ? linkMapper(globals?.content?.header?.[0].cta[0].link, globals?.content?.header?.[0].cta[0].label) : null,
     },
   };
