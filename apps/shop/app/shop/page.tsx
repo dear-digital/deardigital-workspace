4d8644ea297fa60bdf34a823a3eb7745c00b5d36
michiel vandewalle <michiel@deardigital.com>
2023-06-11 10:02:54 +0200

    Add storyblok SDK

--- apps/shop/app/shop/page.tsx
+++ apps/shop/app/shop/page.tsx
@@ -1,13 +1,14 @@
-import { ShopUi } from "@deardigital/shop/ui";
+import { PAGE_TYPES } from '@deardigital/shared/constants';
+import { FetchPageBySlug } from '@deardigital/shared/services';
+import { PageView } from '@deardigital/shared/ui';
 
 /* eslint-disable-next-line */
 export interface ShopProps { }
 
 export default async function Shop(props: ShopProps) {
+  const data = await new FetchPageBySlug(PAGE_TYPES.shop, '').fetch(true);
+
   return (
-    <div>
-      <h1>Welcome to Shop!</h1>
-      <ShopUi />
-    </div>
+    <PageView {...data} />
   );
 }
