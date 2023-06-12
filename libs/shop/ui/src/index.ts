88408cd5dc95ef21a5274c46cc04261deafdbab1
michiel vandewalle <michiel@deardigital.com>
2023-06-12 09:28:09 +0200

    Export components

--- libs/shop/ui/src/index.ts
+++ libs/shop/ui/src/index.ts
@@ -1,2 +1,6 @@
+export * from './lib/header/search-input/search-input';
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
+export * from './lib/page/page';
+export * from './lib/header/header';
 
+export * from './lib/storyblok/storyblok-provider';
