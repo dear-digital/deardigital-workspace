d76814de187b92500a58c8d7966afadacb258d70
michiel vandewalle <michiel@deardigital.com>
2023-06-12 09:26:45 +0200

    export components

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,7 +1,9 @@
 export * from './lib/content-blocks';
 export * from './lib/content-blocks/text';
 export * from './lib/header/header';
+export * from './lib/hero-blocks';
 export * from './lib/page';
+export * from './lib/navigation/navigation-link';
 export * from './lib/section/section';
 export * from './lib/tech-radar/tech-radar';
 
