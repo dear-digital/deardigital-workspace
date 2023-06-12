4d1242aa5aba86c799c490dea7daccff61a56ba2
michiel vandewalle <michiel@deardigital.com>
2023-06-12 09:27:05 +0200

    Create page header

---
+++ libs/shop/ui/src/lib/header/header.tsx
@@ -0,0 +1,48 @@
+import { GlobalInterface } from '@deardigital/shared/interfaces';
+import { NavigationLink } from '@deardigital/shared/ui';
+import { SearchInput } from './search-input/search-input';
+import Link from 'next/link';
+
+/* eslint-disable-next-line */
+export interface HeaderProps {
+  globals: GlobalInterface | null;
+}
+
+export function Header({ globals }: HeaderProps) {
+  return (
+
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link href="/" className="flex items-center">
+          <img src="/brand-logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+          <span className="sr-only">Knoll</span>
+        </Link>
+        <div className="flex md:order-2">
+          <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
+            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            <span className="sr-only">Search</span>
+          </button>
+          <div className="relative hidden md:block">
+            <SearchInput />
+          </div>
+          <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+            <span className="sr-only">Open menu</span>
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+          </button>
+        </div>
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+          <div className="relative mt-3 md:hidden">
+            <SearchInput />
+          </div>
+          {globals?.header && (
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              {globals.header.navigation.map((item, key) => <NavigationLink key={key} {...item} />)}
+            </ul>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
